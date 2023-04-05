8d46862d33f91677bc0ab87ae236c5c88a986c48
raluido <raluido@gmail.com>
2023-04-05 13:09:16 +0100

    again

--- resources/views/user/editData.blade.php
+++ resources/views/user/editData.blade.php
@@ -8,41 +8,39 @@
             Aqui puedes modificar tus datos de usuario y la contraseña
         </div>
         <div class="bottom">
-            <div class="innerBottom">
-                <div class="sideNavButtons">
-                    <button class="stylingButtons gray"><a class="buttonTextWt" href="{{ route('user.editData') }}">Modificar
-                            datos</a>
-                    </button>
-                    <button class="stylingButtons gray"><a class="buttonTextWt" href="{{ route('user.editPassword') }}">Modificar
-                            contraseña</a>
-                    </button>
-                </div>
-                <div class="divForm">
-                    <form method='POST' action='/user/updateData'>
-                        @csrf
-                        <div class="inputForm">
-                            <label for='text' required>Nombre</label>
-                            <input type='name' id='name' name='name' placeholder='Nombre' value='{{ $user->name }}'>
-                            @if ($errors->has('name'))
-                            <span class='text-danger text-left'>{{ $errors->first('name') }}</span>
-                            @endif
-                        </div>
-                        <div class='inputForm'>
-                            <label for='email' required>Email</label>
-                            <input type='email' name='email' id='email' placeholder='Email' value='{{ $user->email }}'>
-                            @if ($errors->has('email'))
-                            <span class='text-danger text-left'>{{ $errors->first('email') }}</span>
-                            @endif
-                        </div>
-                        <div class=''>
-                            <input type='hidden' value='{{ $user->password }}'>
-                        </div>
-                        <div class="buttonsNav">
-                            <button class="stylingButtons blue"><a href="{{ route('home.index') }}" class="buttonTextWt">Volver</a></button>
-                            <button class="stylingButtons green buttonTextWt" type='submit'>Editar</button>
-                        </div>
-                    </form>
-                </div>
+            <div class="sideNavButtons">
+                <button class="stylingButtons gray"><a class="buttonTextWt" href="{{ route('user.editData') }}">Modificar
+                        datos</a>
+                </button>
+                <button class="stylingButtons gray"><a class="buttonTextWt" href="{{ route('user.editPassword') }}">Modificar
+                        contraseña</a>
+                </button>
+            </div>
+            <div class="divForm">
+                <form method='POST' action='/user/updateData'>
+                    @csrf
+                    <div class="inputForm">
+                        <label for='text' required>Nombre</label>
+                        <input type='name' id='name' name='name' placeholder='Nombre' value='{{ $user->name }}'>
+                        @if ($errors->has('name'))
+                        <span class='text-danger text-left'>{{ $errors->first('name') }}</span>
+                        @endif
+                    </div>
+                    <div class='inputForm'>
+                        <label for='email' required>Email</label>
+                        <input type='email' name='email' id='email' placeholder='Email' value='{{ $user->email }}'>
+                        @if ($errors->has('email'))
+                        <span class='text-danger text-left'>{{ $errors->first('email') }}</span>
+                        @endif
+                    </div>
+                    <div class=''>
+                        <input type='hidden' value='{{ $user->password }}'>
+                    </div>
+                    <div class="buttonsNav">
+                        <button class="stylingButtons blue"><a href="{{ route('user.editData') }}" class="buttonTextWt">Volver</a></button>
+                        <button class="stylingButtons green buttonTextWt" type='submit'>Editar</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
--- resources/views/user/editPassword.blade.php
+++ resources/views/user/editPassword.blade.php
@@ -1,63 +1,44 @@
 @extends('layouts.app-master')
 
 @section('content')
-    <div class="bg-light p-4 rounded">
-        <div class="">
+<div class="editPass">
+    <div class="innerEditPass">
+        <div class="top">
             <h1>Usuario</h1>
         </div>
-        <div class="container mt-4">
-            <div class="d-flex">
-                <div class="mt-5 px-4 border">
-                    <div class="mt-3"><a class="text-dark text-decoration-none" href="{{ route('user.editData') }}">Modificar
-                            datos</a>
-                    </div>
-                    <div class="mt-3"><a class="text-dark text-decoration-none"
-                            href="{{ route('user.editPassword') }}">Modificar
-                            contraseña</a>
-                    </div>
-                </div>
-                <div class="">
-                    <div class="d-flex justify-content-center">
-                        <form method='POST' action="{{ route('user.updatePassword') }}">
-                            @csrf
-                            <div class='mt-5 d-flex'>
-                                <div class="row">
-                                    <div class="col-5">
-                                        <label for='password' required>Contraseña</label>
-                                    </div>
-                                    <div class="col-7">
-                                        <input type='password' name='password' id='password' placeholder='Contraseña'
-                                            value=''>
-                                        @if ($errors->has('password'))
-                                            <span class="text-danger text-left">{{ $errors->first('password') }}</span>
-                                        @endif
-                                    </div>
-                                </div>
-                            </div>
-                            <div class='mt-5 d-flex'>
-                                <div class="row">
-                                    <div class="col-5">
-                                        <label for='password_confirmation' required>Repite contraseña</label>
-                                    </div>
-                                    <div class="col-7">
-                                        <input type='password' name='password_confirmation' id='password_confirmation'
-                                            placeholder='Repite contraseña'value=''>
-                                        @if ($errors->has('password_confirmation'))
-                                            <span
-                                                class="text-danger text-left">{{ $errors->first('password_confirmation') }}</span>
-                                        @endif
-                                    </div>
-                                </div>
-                            </div>
-                            <div class=''>
-                                <button class="mt-5" type='submit'>Modificar contraseña</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+        <div class="bottom">
+            <div class="sideNavButtons">
+                <button class="stylingButtons gray"><a class="buttonTextWt" href="{{ route('user.editData') }}">Modificar
+                        datos</a>
+                </button>
+                <button class="stylingButtons gray"><a class="buttonTextWt" href="{{ route('user.editPassword') }}">Modificar
+                        contraseña</a>
+                </button>
             </div>
-            <div class="mt-4">
-                <a href="{{ route('home.index') }}" class="btn btn-secondary">Volver</a>
+            <div class="divForm">
+                <form method='POST' action="{{ route('user.updatePassword') }}">
+                    @csrf
+                    <div class='inputForm'>
+                        <label for='password' required>Contraseña</label>
+                        <input type='password' name='password' id='password' placeholder='Contraseña' value=''>
+                        @if ($errors->has('password'))
+                        <span class="text-danger text-left">{{ $errors->first('password') }}</span>
+                        @endif
+                    </div>
+                    <div class="inputForm">
+                        <label for='password_confirmation' required>Repite contraseña</label>
+                        <input type='password' name='password_confirmation' id='password_confirmation' placeholder='Repite contraseña' value=''>
+                        @if ($errors->has('password_confirmation'))
+                        <span class="text-danger text-left">{{ $errors->first('password_confirmation') }}</span>
+                        @endif
+                    </div>
+                    <div class="buttonsNav">
+                        <button class="stylingButtons blue"><a href="{{ route('home.index') }}" class="buttonTextWt">Volver</a></button>
+                        <button class="stylingButtons green buttonTextWt" type='submit'>Modificar contraseña</button>
+                    </div>
+                </form>
             </div>
         </div>
-    @endsection
+    </div>
+</div>
+@endsection
