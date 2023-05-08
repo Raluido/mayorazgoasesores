681d8d9beb157f5fe40a76fdc911b784a7aa6de9
raluido <raluido@gmail.com>
2023-05-08 22:44:04 +0100

    again

--- resources/views/layouts/auth-master.blade.php
+++ resources/views/layouts/auth-master.blade.php
@@ -5,7 +5,7 @@
     <title>mayorazgo asesores</title>
     <meta name="author" content="Websiwebs.es">
     <meta charset="utf-8">
-    <meta name="description" content="Asesoria laboral en Tenerife">
+    <meta name="description" content="Asesoria laboral en La Orotava, Tenerife">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="keywords" content="Seguros sociales, nóminas, asesoramiento laboral, contratos, bonificaciones, despidos, finiquitos, altas y bajas por enfermedad y accidentes de trabajo">
